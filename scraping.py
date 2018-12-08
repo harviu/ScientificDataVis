317b01cb04df29f6fe347582b619fd9fd407fa92
Harvey Li <aide0740@gmail.com>
2018-12-08 17:14:03 -0500

    new scripting

--- scraping.py
+++ scraping.py
@@ -1,15 +1,41 @@
 import urllib.request
 import re
+from multiprocessing import Pool
+from pathlib import Path
+from progress.bar import Bar
 
 
-def download(dataName):
+def download(url):
+    filename = "output/"+url[-9:]
+    file = Path(filename)
+    if not file.is_file():
+        count = 0
+        while True:
+            # print("Downloading "+filename)
+            try:
+                urllib.request.urlretrieve(url,filename)
+                break
+            except:
+                print("Retrying...")
+                count = count +1
+                if count == 5:
+                    print("Failed to download "+filename)
+                    file.unlink()
+                    break
+
+
+
+def main():
     p = re.compile(r'http://oceans11.*?\.vti')
-    with open(dataName+".html",'r',encoding='utf-8') as html:
+    with open("yA31.html",'r',encoding='utf-8') as html:
         text = html.read()
-        for url in p.finditer(text):
-            filename = url[0][-9:] #url[0] is the match 
-            urllib.request.urlretrieve(url[0],dataName+"/"+filename)
+        url = p.findall(text)
+        bar = Bar('Downloading',max = len(url))
+        pool = Pool(processes=1) # change the para task number
+        for _ in pool.imap(download, url):
+            bar.next()
+        bar.finish()
 
-#uncomment the line to download the data
-# download("yA31")
-# download("yC31")
+
+if __name__ == '__main__':
+    main()
